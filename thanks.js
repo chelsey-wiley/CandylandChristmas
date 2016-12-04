22dcae57daa6eb7e52492ba658444801ef914c7e
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-04 00:33:12 -0600

    created thanks.html. Added the city of Lancaster links as well as a back to index button. Styled thanks.html with its own stylesheet.

---
+++ thanks.js
@@ -0,0 +1,9 @@
+console.log('thanks')
+
+var home =
+document.querySelector('.send-again button');
+
+home.addEventListener('click', function(){
+console.log('yes clicked')
+window.location.href = 'index.html';
+});
