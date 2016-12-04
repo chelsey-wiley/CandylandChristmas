114497780e74f8e204d76918ed6ce5df11bb72d0
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-03 21:53:54 -0600

    querySelectorAll not working on wishlist. Updated sending message and buttons.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log('hellow')
+
+// var clickHere = document.querySelector('.toy-box li');
+//
+// clickHere.addEventListener('click', function(){
+//
+//     var checkMark = document.querySelector('.toy-box li');
+//     console.log('clicked')
+//
+//     checkMark.classList.toggle('check-mark')
+//
+// });
+
+
+var yes =
+document.querySelector('.yes');
+
+yes.addEventListener('click', function(){
+console.log('yes clicked')
+
+var message =
+document.querySelector('.confirm');
+message.innerHTML = '<p>Your message has been sent.</p>'
+
+});
