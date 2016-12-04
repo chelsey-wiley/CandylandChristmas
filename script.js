af7a11a61ab2f5dbf70ea6b2bf10716dc4993d1f
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-03 23:34:18 -0600

    changed navigation arrows. Bug in querySelectorAll on wishlist.html and in JS script file. Removed stylings from style.css to create individual stylesheets for pages.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-console.log('hellow')
-
+console.log('script')
+// 
 // var clickHere = document.querySelector('.toy-box li');
 //
 // clickHere.addEventListener('click', function(){
@@ -10,16 +10,3 @@ console.log('hellow')
 //     checkMark.classList.toggle('check-mark')
 //
 // });
-
-
-var yes =
-document.querySelector('.yes');
-
-yes.addEventListener('click', function(){
-console.log('yes clicked')
-
-var message =
-document.querySelector('.confirm');
-message.innerHTML = '<p>Your message has been sent.</p>'
-
-});
