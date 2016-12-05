111f7be32e6d641d268bedfd8ecfbca118f8247e
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-04 22:03:29 -0600

    typo corrected on sending.html. Attempting to git Js to work on check boxes by removing the current image and replacing it with a checkmark.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-console.log('script')
-// 
-// var clickHere = document.querySelector('.toy-box li');
+var grid = document.querySelector('.toy-box');
+console.log('grid', grid);
+
+grid.addEventListener('click', function(evt){
+console.log('clicked', evt.target);
+
+var checkMark =
+document.querySelector('.check-mark');
+
+if (evt.target !==null) {
+  console.log('removed image', evt.target);
+
+// var theCheck =
+// document.querySelector('.check-mark');
+// theCheck.innerHTML = '<img src="check.gif" alt ="check mark">'
 //
-// clickHere.addEventListener('click', function(){
-//
-//     var checkMark = document.querySelector('.toy-box li');
-//     console.log('clicked')
-//
-//     checkMark.classList.toggle('check-mark')
-//
-// });
+// theCheck.classList.remove('checkMark');
+}
+});
