678b8099a5fcb02e91bc84d26b6564a17bf21376
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-05 14:06:24 -0600

    removed clicked class and addeck check in the DOM

--- script.js
+++ script.js
@@ -2,20 +2,13 @@ var grid = document.querySelector('.toy-box');
 console.log('grid', grid);
 
 grid.addEventListener('click', function(evt){
-console.log('clicked', evt.target);
+  console.log('clicked', evt.target);
 
-if (evt.target.tagName === "IMG"){
-  function changeImage(theImage){
-var theImage= document.images.src = "check.gif";
+  if (evt.target.tagName === "IMG"){
+
+    evt.target.src = "check.gif";
 
   }
-
-
-
-
-
-
-   }
 
 
 });
